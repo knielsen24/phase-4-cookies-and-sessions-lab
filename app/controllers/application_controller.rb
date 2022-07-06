418734b633d3333668c71b7fc3b78d18352aeef6
Kevin Nielsen <knielsen24@gmail.com>
2022-07-06 14:58:29 -0700

    commit tree 7f2976e46cb2d50fa85fab503579db042f5898c6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::API
-  include ActionController::Cookies
+  	include ActionController::Cookies
+
+	# # cookies[:user_id] ||= 0
+	# session[:page_views] ||= 0
+	# session[:page_views] += 1
+
 
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,33 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-  def index
-    articles = Article.all.includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
-  end
+	def index
+		articles = Article.all.includes(:user).order(created_at: :desc)
+		render json: articles, each_serializer: ArticleListSerializer
+	end
 
-  def show
-    article = Article.find(params[:id])
-    render json: article
-  end
+	def show
+		article = Article.find(params[:id])
+		session[:page_views] ||= 0
+		session[:page_views] += 1
 
-  private
+		# session[:page_views] < 4 ?
+		# render json: article :
+		# render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
 
-  def record_not_found
-    render json: { error: "Article not found" }, status: :not_found
-  end
+		if session[:page_views] < 4
+			render json: article
+		else
+			render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+		end
+	end
+
+	private
+
+	def record_not_found
+		render json: { error: "Article not found" }, status: :not_found
+	end
 
 end
+
+
